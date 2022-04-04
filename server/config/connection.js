d7c3bb9c82047d9ffaf229c1735e5f10ed5875ad
Terrell Hudson <thudson5731@gmail.com>
2022-04-04 09:39:36 -0400

    adding .env files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect('mongodb://localhost/googlebooks' || process.env.mongo_db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
