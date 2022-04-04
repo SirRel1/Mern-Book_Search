39159f82329ee02f787f0de7584839608121d944
Terrell Hudson <thudson5731@gmail.com>
2022-04-04 09:24:29 -0400

    new files and gitignore for upload and build

---
+++ server/controllers/Book.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose');
+// Saved books array schema
+const bookSchema = new Schema({
+  authors: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  // saved book id from GoogleBooks
+  bookId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = bookSchema;
