8d643ba765dd02e9a9430f129d33bfe06cfcd239
Terrell Hudson <thudson5731@gmail.com>
2022-04-04 10:52:27 -0400

    verifying connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.mongo_db || "mongodb+srv://SirRel1:2wiceBorn@cluster0.zqhlr.mongodb.net/googlebooks?retryWrites=true&w=majority" , {
+mongoose.connect(process.env.mongo_db || "mongodb+srv://Sirel1:2wiceBorn@cluster0.9jiqw.mongodb.net/googlebooks?retryWrites=true&w=majority" , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
